0a56c2d61041ae25b6760ad804bd8cb3128b7f48
nmsulliv <nicolesullivanmarie@gmail.com>
2020-06-27 18:42:03 -0700

    Add example of shortest path

--- degrees.py
+++ degrees.py
@@ -89,6 +89,14 @@ def shortest_path(source, target):
     Returns the shortest list of (movie_id, person_id) pairs
     that connect the source to the target.
 
+    Example:
+    Name: Emma Watson
+    Name: Jennifer Lawrence
+    3 degrees of separation.
+    1: Emma Watson and Brendan Gleeson starred in Harry Potter and the Order of the Phoenix
+    2: Brendan Gleeson and Michael Fassbender starred in Trespass Against Us
+    3: Michael Fassbender and Jennifer Lawrence starred in X-Men: First Class
+
     If no possible path, returns None.
     """
 
