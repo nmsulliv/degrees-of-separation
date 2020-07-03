d382c4cd94084a5c7bb6b91232fec438147dda86
nmsulliv <nicolesullivanmarie@gmail.com>
2020-07-03 13:57:22 -0700

    Add utilities class

--- mysite/processing.py
+++ mysite/processing.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-from util import Node, StackFrontier, QueueFrontier
+from utilities import Node, StackFrontier, QueueFrontier
 
 # Maps names to a set of corresponding person_ids
 names = {}
