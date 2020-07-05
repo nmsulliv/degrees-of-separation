80522acd8ed4f43ff2a280fd8cef821aaa088bee
nmsulliv <nicolesullivanmarie@gmail.com>
2020-07-04 17:18:15 -0700

    Load dicts from pickle file

--- mysite/processing.py
+++ mysite/processing.py
@@ -1,43 +1,55 @@
 import pickle
 from utilities import Node, QueueFrontier
 
-def load_data(directory):
-    """
-    Load data from CSV files into memory.
-    """
-    # Load people
-    with open(f"{directory}/people.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            people[row["id"]] = {
-                "name": row["name"],
-                "birth": row["birth"],
-                "movies": set()
-            }
-            if row["name"].lower() not in names:
-                names[row["name"].lower()] = {row["id"]}
-            else:
-                names[row["name"].lower()].add(row["id"])
+# Maps names to a set of corresponding person_ids
+pickle_in_n = open("/Users/nicolesullivan/Code/degrees/mysite/static/pickle/namesdict.pickle","rb")
+names = pickle.load(pickle_in_n)
 
-    # Load movies
-    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            movies[row["id"]] = {
-                "title": row["title"],
-                "year": row["year"],
-                "stars": set()
-            }
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+pickle_in_p = open("/Users/nicolesullivan/Code/degrees/mysite/static/pickle/peopledict.pickle","rb")
+people = pickle.load(pickle_in_p)
 
-    # Load stars
-    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            try:
-                people[row["person_id"]]["movies"].add(row["movie_id"])
-                movies[row["movie_id"]]["stars"].add(row["person_id"])
-            except KeyError:
-                pass
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+pickle_in_m = open("/Users/nicolesullivan/Code/degrees/mysite/static/pickle/moviesdict.pickle","rb")
+movies = pickle.load(pickle_in_m)
+
+# def load_data(directory):
+#     """
+#     Load data from CSV files into memory.
+#     """
+#     # Load people
+#     with open(f"{directory}/people.csv", encoding="utf-8") as f:
+#         reader = csv.DictReader(f)
+#         for row in reader:
+#             people[row["id"]] = {
+#                 "name": row["name"],
+#                 "birth": row["birth"],
+#                 "movies": set()
+#             }
+#             if row["name"].lower() not in names:
+#                 names[row["name"].lower()] = {row["id"]}
+#             else:
+#                 names[row["name"].lower()].add(row["id"])
+
+#     # Load movies
+#     with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+#         reader = csv.DictReader(f)
+#         for row in reader:
+#             movies[row["id"]] = {
+#                 "title": row["title"],
+#                 "year": row["year"],
+#                 "stars": set()
+#             }
+
+#     # Load stars
+#     with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+#         reader = csv.DictReader(f)
+#         for row in reader:
+#             try:
+#                 people[row["person_id"]]["movies"].add(row["movie_id"])
+#                 movies[row["movie_id"]]["stars"].add(row["person_id"])
+#             except KeyError:
+#                 pass
 
 def display_result(source, target):
     pathway = ""
