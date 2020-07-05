0d2c9a544380c704fae058da95fceac2d26cf4c8
nmsulliv <nicolesullivanmarie@gmail.com>
2020-07-04 17:24:20 -0700

    Remove entire load data

--- mysite/processing.py
+++ mysite/processing.py
@@ -13,44 +13,6 @@ people = pickle.load(pickle_in_p)
 pickle_in_m = open("/Users/nicolesullivan/Code/degrees/mysite/static/pickle/moviesdict.pickle","rb")
 movies = pickle.load(pickle_in_m)
 
-# def load_data(directory):
-#     """
-#     Load data from CSV files into memory.
-#     """
-#     # Load people
-#     with open(f"{directory}/people.csv", encoding="utf-8") as f:
-#         reader = csv.DictReader(f)
-#         for row in reader:
-#             people[row["id"]] = {
-#                 "name": row["name"],
-#                 "birth": row["birth"],
-#                 "movies": set()
-#             }
-#             if row["name"].lower() not in names:
-#                 names[row["name"].lower()] = {row["id"]}
-#             else:
-#                 names[row["name"].lower()].add(row["id"])
-
-#     # Load movies
-#     with open(f"{directory}/movies.csv", encoding="utf-8") as f:
-#         reader = csv.DictReader(f)
-#         for row in reader:
-#             movies[row["id"]] = {
-#                 "title": row["title"],
-#                 "year": row["year"],
-#                 "stars": set()
-#             }
-
-#     # Load stars
-#     with open(f"{directory}/stars.csv", encoding="utf-8") as f:
-#         reader = csv.DictReader(f)
-#         for row in reader:
-#             try:
-#                 people[row["person_id"]]["movies"].add(row["movie_id"])
-#                 movies[row["movie_id"]]["stars"].add(row["person_id"])
-#             except KeyError:
-#                 pass
-
 def display_result(source, target):
     pathway = ""
     result = shortest_path(source, target)
